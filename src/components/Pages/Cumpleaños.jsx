2b5d084484d1a7f956a9bc5673da0cf19739ec01
emilianoolbe <emilianoolbe@gmail.com>
2023-04-01 12:58:40 -0300

    primero cambios

--- src/components/Pages/Cumpleaños.jsx
+++ src/components/Pages/Cumpleaños.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
-export const Cumpleaños = ({pagina}) => {
+export const Cumpleaños = ({ pagina }) => {
   //Estados
   const [funcionarios, setFuncionarios] = useState([]);
   const [errores, setErrores] = useState("");
@@ -20,53 +20,60 @@ export const Cumpleaños = ({pagina}) => {
       const PETICION = await fetch(url);
       const { data } = await PETICION.json();
       setFuncionarios(data);
-      setErrores('');
-
+      setErrores("");
     } catch (err) {
       setErrores(`Error en la petición de funcionarios`);
     }
   };
 
-  if (errores !== '') {
-    return(
-      <div className=" container row justify-content-around align-items-center mt-4 mb-4">
-        <h3 className="text-center">Cumpleaños del mes</h3>
-         <p className="fs-4 fw-bold text-center error-mensaje"> No se han podido cargar los cumpleaños {errores}</p>
-      </div>
-    )
-
-  }else if(errores === ''){
+  if (errores !== "") {
     return (
       <div className=" container row justify-content-around align-items-center mt-4 mb-4">
         <h3 className="text-center">Cumpleaños del mes</h3>
-  
+        <p className="fs-4 fw-bold text-center error-mensaje">
+          {" "}
+          No se han podido cargar los cumpleaños {errores}
+        </p>
+      </div>
+    );
+  } else if (errores === "") {
+    return (
+      <div className=" container row justify-content-around align-items-center mt-4 mb-4">
+        <h3 className="text-center">Cumpleaños del mes</h3>
+
         {funcionarios.map((empleado, i) => {
           return (
-            <div className="col-md-4 col-sm-12  me-5 row justify-content-center aling-items-center" key={i}>
-              <div className="col-4 w-40  align-self-center">
-              <Card  className="card- m-2 bg-dark text-white" border="light">
-                <Card.Img src={empleado.avatar} />
-              </Card>
-              </div>
-              <div className="col-8">
-
-              <Card style={{ width: "14rem" }} className="card- m-2 bg-dark text-white " border="light">
-          
-                  <Card.Body clas>
-                    <Card.Header className="text-center fw-bold">
-                      {empleado.first_name} {empleado.last_name}{" "}
+            <div className="col-xxl-3 col-lg-4 col-md-6 col-sm-12  me-5 row justify-content-center aling-items-center" key={i} >
+              <div>
+                <Card
+                  style={{ width: "14rem" }}
+                  className="card"
+                  border="light"
+                >
+                  <div>
+                    <Card.Img src={empleado.avatar} />
+                  </div>
+                  
+                  <div className="ms-1 card-body">
+                    
+                  <Card.Body>
+                    <Card.Header className="text-center fw-bold bg text-light">
+                      {empleado.first_name} {empleado.last_name}
                     </Card.Header>
-                    <ListGroup >
-                      <ListGroup.Item className="bg-dark text-white">Edad: </ListGroup.Item>
-                      <ListGroup.Item className="bg-dark text-white">Fecha:</ListGroup.Item>
-                      <ListGroup.Item className="bg-dark text-white">Sector: Desarrollo</ListGroup.Item>
+                    <ListGroup className="text-dark fw-bold list" border="light">
+                      <ListGroup.Item>Edad: </ListGroup.Item>
+                      <ListGroup.Item>Fecha:</ListGroup.Item>
+                      <ListGroup.Item>Sector: Desarrollo</ListGroup.Item>
+                      <ListGroup.Item>Cargo:</ListGroup.Item>
+      
                     </ListGroup>
                   </Card.Body>
-            
-              </Card>
+                  </div>
+
+                </Card>
               </div>
-          </div>
-  
+             
+            </div>
           );
         })}
       </div>
